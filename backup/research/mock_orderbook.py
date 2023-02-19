0f7fbf5120791afe85e08a1ff0d2f8d3b29ff305
subzero <erma0x>
2023-02-19 18:41:47 +0100

    change reseach project structure

--- backup/research/mock_orderbook.py
+++
@@ -1,8 +0,0 @@
-# TO DO
-# get_orderbook_depth_demo() = FAKE from SQL 
-# get data each diff timestamp from datapoint and stream to the demo_oracle
-# modify oracle.py in
-# line 87        orderbook = get_orderbook_depth(ticker=MARKET, limit_=LIMIT_ORDER_BOOK)
-# per prendere i dati get_orderbook_depth_demo()
-
-
